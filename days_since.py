f0f22f351f767d0c301de9187b2f6bd6fbc7409a
Marisa Hoenig <marisa.hoenig@gmail.com>
2015-12-27 20:32:25 -0500

    Finished comments and all solving.

--- days_since.py
+++ days_since.py
@@ -2,46 +2,47 @@
 # Assume that the birthday and current date are correct dates (& no time travel).
 # Simply put, if you were born 1 Jan 2012 and today's date is 2 Jan 2012, you are 1 day old.
 
+# Marisa Hoenig
+# Udacity Problem Solving
 
 daysOfMonths = [ 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
-def isLeapYear(year):
+def isLeapYear(year):  # takes the year as input and edits the daysOfMonths to have Feb = 29 if it is a leap year
     if year%4 == 0:
         daysOfMonths[1] = 29
     else:
         daysOfMonths[1] = 28
     return daysOfMonths
 
-def findDays(birthday, current_date):
-    b_month = int(birthday[0:2])
+def findDays(birthday, current_date): #takes birthday and current_date as inputs
+    b_month = int(birthday[0:2]) #converts the strings to ints, based on the input
     b_day = int(birthday[3:5])
     b_year = int(birthday[6:])
     c_month = int(current_date[0:2])
     c_day = int(current_date[3:5])
     c_year = int(current_date[6:])
 
-
-    if b_year == c_year and b_month == c_month:
-        age = c_day - b_day
-    else:
+    if b_year == c_year and b_month == c_month: # if birth month and year  are equal to the current month and year,
+        age = c_day - b_day                     # it subtracts the days
+    else: # finds the leftover days in a month (ex: if birthday is the 17th of a month, it returns the days remaining in the month)
         year = b_year
         days = isLeapYear(year)
         leftover_days_in_month = days[b_month-1] - b_day
         age = leftover_days_in_month
         month = b_month + 1
-    while year < c_year:
+    while year < c_year: # while the year is less than current year
         days = isLeapYear(year)
-        age += days[month-1]
-        month +=1
-        if month == 13:
+        age += days[month-1] # finds number of days in the month and adds to age
+        month +=1 # iterates through each month
+        if month == 13: # if month is 13, it reassigns it to 1 (January)
             month = 1
-            year += 1
-    while year == c_year and month != c_month:
-        age += daysOfMonths[month-1]
-        month += 1
-    age += c_day
+            year += 1 # new year
+    while year == c_year and month != c_month: #if it is the same year and not same month
+        age += daysOfMonths[month-1] #adds number of days in month
+        month += 1 # iterates through months
+    age += c_day # else, when in same year and month as current, it adds those days to age
 
-    return age
+    return age # returns age in days
 
 
 print findDays('05/10/1990', '12/27/2015')
